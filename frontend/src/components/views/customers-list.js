aefa2971356af6705cbc34467f820df5dcbf68c2
Idraki Muhamad <idrakimuhamad@gmail.com>
2017-02-19 23:40:03 +0800

    Change the path for first stage

--- frontend/src/components/views/customers-list.js
+++ frontend/src/components/views/customers-list.js
@@ -10,7 +10,7 @@ export default function(props) {
         return (
           <Link
             key={customer.id}
-            to={'/customers/' + customer.id}
+            to={'/browse?customer=' + customer.id}
             className="f6 link dim br-pill ba bw1 ph3 pv2 mb2 ml2 mr2 dib black ttc">
             {customer.name}
           </Link>
