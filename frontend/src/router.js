cff83d8b5bc9129df81b4782507901cf6323c4ec
Idraki Muhamad <idrakimuhamad@gmail.com>
2017-02-19 23:12:41 +0800

    Restructure routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -10,17 +10,16 @@ import {
 import MainLayout from './components/layouts/main-layout';
 
 // Pages
-import About from './components/pages/About';
+import Home from './components/pages/Home';
 
 // Containers
-import CustomerListContainer from './containers/customer-list-container';
+// import CustomerListContainer from './containers/customer-list-container';
 
 export default (
 	<Router history={browserHistory}>
 		<Route component={MainLayout}>
 			<Route path="/">
-				<IndexRoute component={CustomerListContainer}></IndexRoute>
-				<Route path="about" component={About} />
+				<IndexRoute component={Home}></IndexRoute>
 			</Route>
 		</Route>
 	</Router>
