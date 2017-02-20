2d575238a40f72386e73bc2a5d28e0336244a31e
Idraki Muhamad <idrakimuhamad@gmail.com>
2017-02-20 17:05:12 +0800

    Bring out the header as its own component

--- frontend/src/components/views/app-header.js
+++ frontend/src/components/views/app-header.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default (props) => {
   return (
-    <header className='tc pv4 pv5-ns bg-mid-gray'>
+    <header className='tc pv4 pv5-ns bg-mid-gray app-header'>
       <h1 className='f5 f4-ns fw6 near-white'>Ad Shop</h1>
     </header>
   );
