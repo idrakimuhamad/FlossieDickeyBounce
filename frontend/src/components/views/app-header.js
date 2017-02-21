1d783c9cff8d2e49201408e0da1948dbc2dd7905
Idraki Muhamad <idrakimuhamad@gmail.com>
2017-02-22 01:12:30 +0800

    Added anchor to the header

--- frontend/src/components/views/app-header.js
+++ frontend/src/components/views/app-header.js
@@ -1,10 +1,17 @@
 import React from 'react';
-// import { Link } from 'react-router';
 
 export default (props) => {
   return (
     <header className='tc pv4 pv5-ns bg-mid-gray app-header'>
-      <h1 className='f5 f4-ns fw6 near-white'>Ad Shop</h1>
+      <h1 className='f5 f4-ns fw6'>
+        {/* instead of making this a react router link,
+        we purposely use anchor to serve as a page refresh to
+        clear all states */}
+        <a href="/"
+          className="link dim near-white">
+          Ad Shop
+        </a>
+      </h1>
     </header>
   );
 };
