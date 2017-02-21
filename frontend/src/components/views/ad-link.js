37f21debb3231f9807878ee663d2e1895773cb2c
Idraki Muhamad <idrakimuhamad@gmail.com>
2017-02-22 01:13:08 +0800

    Give a class name to the ad link anchor

--- frontend/src/components/views/ad-link.js
+++ frontend/src/components/views/ad-link.js
@@ -5,7 +5,7 @@ export default function(props) {
   return (
 		<div className="dtc-ns pa2">
       <a href=""
-        className="db link black"
+        className="ad-item db link black"
         onClick={props.addToCart(props.ad.id)}>
         <div className="w-100 bg-white br3 pa3 pa4-ns mv3 ba b--black-10 grow">
           <div className="tc">
