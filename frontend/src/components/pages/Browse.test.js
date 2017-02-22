c982acd4e0623d44a61c7ce920ef1a0334efa361
Idraki Muhamad <idrakimuhamad@gmail.com>
2017-02-22 17:19:28 +0800

    Adjust the browse page test

--- frontend/src/components/pages/Browse.test.js
+++ frontend/src/components/pages/Browse.test.js
@@ -2,20 +2,29 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Browse from './Browse';
 
+function setup() {
+	const props = {
+		location: {
+			query: {
+				customer: 1
+			}
+		}
+	};
+	
+	const wrapper = shallow(<Browse {...props} />)
+
+  return {
+    props,
+    wrapper
+  };
+}
+
 describe('components', () => {
 	describe('Browse', () => {
 		it('Should render itself', () => {
-			const location = {
-				query: {
-					customer: 1
-				}
-			}
-
-			const wrapper = shallow(<Browse location={location} />)
-
+			const { wrapper } = setup();
+			
 			expect(wrapper.find('div').hasClass('browse-page')).toBe(true);
-			expect(wrapper.find('h1').text()).toBe('Browse');
-			expect(wrapper.find('p').text()).toBe('Pick the ad package to be added into your cart');
 		});
 	});
 });
