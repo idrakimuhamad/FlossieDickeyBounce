480c6e0d203aed732f6f51aaa8af98d80141d4f6
Idraki Muhamad <idrakimuhamad@gmail.com>
2017-02-22 15:19:06 +0800

    Change the implementation
    To show the selected customer name

--- frontend/src/components/containers/browse-container.js
+++ frontend/src/components/containers/browse-container.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getAds, addToCart } from '../../api/ads-api';
+import { getCustomer } from '../../api/customer-api';
 import AdsList from '../views/ads-list';
 import Button from '../views/button';
 
 export class BrowseContainer extends Component {	
 	componentDidMount() {
 		getAds();
+		getCustomer(this.props.customerId);
 	}
 
 	_addToCart = (adId) => (e) => {
@@ -31,19 +33,26 @@ export class BrowseContainer extends Component {
 																			+ (total > 1 ? 's' : '')
 																			+ ' in the cart' : 'No item added yet';
     return (
-      <div>
-				<AdsList
-					loading={this.props.cartLoading}
-					ads={this.props.ads}
-					addToCart={this._addToCart} />
-				<div className="pv2">
-					<Button
-						href={checkoutUrl}
-						title={checkoutTitle}
-						text={checkoutText}
-						disabled={canCheckout}
-						bgColorClass="bg-mid-gray"
-						/>
+      <div>				
+        <h1 className="f3 lh-copy">Browse (as <span className="ttc">{this.props.customer.name}</span>)</h1>
+        <p className="f5 lh-copy">
+          Pick the ad package to be added into your cart
+        </p>
+				<div className="ads-list-container">
+					<AdsList
+						customer={this.props.customer}
+						loading={this.props.cartLoading}
+						ads={this.props.ads}
+						addToCart={this._addToCart} />
+					<div className="pv2">
+						<Button
+							href={checkoutUrl}
+							title={checkoutTitle}
+							text={checkoutText}
+							disabled={canCheckout}
+							bgColorClass="bg-mid-gray"
+							/>
+					</div>
 				</div>
       </div>
     );
@@ -56,7 +65,7 @@ const mapStateToProps = function(store) {
 		cart: store.adsState.selectedAds,
 		ads: store.adsState.ads,
 		// customerId: store.customersState.customerId,
-		// customer: store.customersState.customer,
+		customer: store.customersState.customer,
 		// customerLoading: store.customersState.customerLoading
 	};
 };
