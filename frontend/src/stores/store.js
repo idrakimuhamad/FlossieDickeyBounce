8fa6f96139b2aef4051b33a946715fd062ef2227
Idraki Muhamad <idrakimuhamad@gmail.com>
2017-02-23 11:27:19 +0800

    Added redux logger

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -1,5 +1,16 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import reducers from '../reducers';
-const store = createStore(reducers);
+
+const middlewares = [];
+
+// only log in dev
+if (process.env.NODE_ENV === 'development') {
+  const createLogger = require('redux-logger');
+  const logger = createLogger();
+  middlewares.push(logger);
+}
+
+const store = createStore(reducers, applyMiddleware(...middlewares));
+
 
 export default store;
